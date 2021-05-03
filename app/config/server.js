05d85ea823639f4e20a4137c96a888888f9a2e4b
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-05-03 16:14:43 +0000

    first commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8260a04d7eb75ada44010334aac07fc1'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bcae9ff7-e251-4f54-8201-c1ca44ae96c0'
+};
